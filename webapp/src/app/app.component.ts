92aa18f1b90dda5c8c2012eedb6f8d75667976f1
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-05 23:34:00 +0400

    chore: fill styles.scss with mixines and variables

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core'
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'webapp'
-}
+export class AppComponent {}
