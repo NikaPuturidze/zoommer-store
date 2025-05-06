137a4d1ad12bbf8185ab789ab5c840f28f0bf235
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-06 18:07:58 +0400

    chore(commitlint): update rules for subject and type

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,6 +10,6 @@ module.exports = {
     'subject-empty': [2, 'never'],
     'header-max-length': [2, 'always', 72],
     'type-case': [2, 'always', 'lower-case'],
-    'scope-empty': [1, 'always'],
+    'scope-empty': [0, 'always'],
   },
 }
