65b0183749241398a2c21a716ae1e8948722e56f
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-09 22:49:26 +0400

    feat(footer): finished footer

--- webapp/src/app/footer/footer.component.ts
+++ webapp/src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core'
 import { ITopicsResponse } from '../interfaces/topics.interface'
+import { LanguageService } from '../services/language.service'
 
 @Component({
   selector: 'app-footer',
@@ -9,8 +10,23 @@ import { ITopicsResponse } from '../interfaces/topics.interface'
 })
 export class FooterComponent implements OnChanges {
   @Input() topics?: ITopicsResponse
+  public currentLang: 'en' | 'ka' = 'en'
+  public optionsArray: string[] = []
+
+  constructor(private languageService: LanguageService) {}
 
   ngOnChanges(): void {
-    console.log(this.topics)
+    this.languageService.currentLanguage$.subscribe((language) => {
+      this.currentLang = language
+      this.updateLanguageState()
+    })
+  }
+
+  private updateLanguageState(): void {
+    if (this.currentLang === 'ka') {
+      this.optionsArray = ['ნავიგაცია', 'გადახდები', 'გამოგვყევი', 'კონტაქტი', 'ფილიალები', 'კორპორატიული გაყიდვები']
+    } else {
+      this.optionsArray = ['Navigation', 'Payments', 'Follow Us', 'Contact', 'Branches', 'Corporate Sales']
+    }
   }
 }
