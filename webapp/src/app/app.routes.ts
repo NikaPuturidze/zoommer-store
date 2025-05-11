ebe6cee4241d0ff2a94ea43e84417db09b639666
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-11 10:42:49 +0400

    feat(routing): add route for products component

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router'
 import { InfoComponent } from './info/info.component'
 import { MainComponent } from './main/main.component'
+import { ProductsComponent } from './products/products.component'
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'info/:topic',
     component: InfoComponent,
+  },
+  {
+    path: ':products',
+    component: ProductsComponent,
   },
 ]
