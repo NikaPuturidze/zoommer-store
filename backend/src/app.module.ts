878432f9079cddfe1ae7c47f9e2965a57c17d74d
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-13 06:13:39 +0400

    fix: return of app.module

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TopicsModule } from './topics/topics.module'
+import { MegaMenuModule } from './mega-menu/mega-menu.module'
+import { ContentModule } from './content/content.module'
+import { TopicModule } from './topic/topic.module'
+import { ProductsModule } from './products/products.module'
+import { FilterModule } from './filter/filter.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TopicsModule,
+    MegaMenuModule,
+    ContentModule,
+    TopicModule,
+    ProductsModule,
+    FilterModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
