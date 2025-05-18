7b3b504b7681d31dedbd9453af2ee8f570b3cb62
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-18 14:03:26 +0400

    feat(template): add inputs to be able to send data separately

--- webapp/src/app/templates/templ-product/templ-product.component.ts
+++ webapp/src/app/templates/templ-product/templ-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { IProduct } from '../../interfaces/content.interface'
 import { LocalStorageService } from '../../services/localstorage.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-templ-product',
@@ -11,13 +12,29 @@ import { LocalStorageService } from '../../services/localstorage.service'
 export class TemplProductComponent implements OnInit {
   @Input() product?: IProduct
   @Input() currentLang: 'en' | 'ka' = 'en'
+  @Input() route?: string[]
+  @Input() imageUrl?: string
+  @Input() productPrice?: number
+  @Input() productSalePrice?: number
+  @Input() productName?: string
+  @Input() labelText?: string
   public showGrid?: string
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.localStorageService.observe('showGrid').subscribe((value) => {
       this.showGrid = value as string | undefined
+    })
+  }
+
+  public navigate(route: string[] | undefined): void {
+    if (!route) return
+    this.router.navigate(route).catch((error: unknown) => {
+      console.error(error)
     })
   }
 
