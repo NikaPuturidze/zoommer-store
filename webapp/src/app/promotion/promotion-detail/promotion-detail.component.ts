091949e290ede55b28e39d741bbd0f4d19ce396b
NikaPuturidze <nikushaputuridze@gmail.com>
2025-05-26 17:43:07 +0400

    feat: add promotion component

---
+++ webapp/src/app/promotion/promotion-detail/promotion-detail.component.ts
@@ -0,0 +1,102 @@
+import { AfterViewChecked, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { ApiService } from '../../services/api.service'
+import { LanguageService } from '../../services/language.service'
+import { IPromotionDetailsResponse } from '../../interfaces/promotion-details.interface'
+import { TemplProductComponent } from '../../templates/templ-product/templ-product.component'
+
+@Component({
+  selector: 'app-promotion-detail',
+  imports: [TemplProductComponent],
+  templateUrl: './promotion-detail.component.html',
+  styleUrl: './promotion-detail.component.scss',
+})
+export class PromotionDetailComponent implements OnInit, AfterViewChecked {
+  public currentLang: 'ka' | 'en' = 'en'
+  public promotionDetail?: IPromotionDetailsResponse
+  public categoryIds: number[] = []
+  public justifyContent: 'center' | 'flex-start' = 'flex-start'
+  public showLoader?: boolean
+  private page = 1
+  private promotionId?: number
+
+  @ViewChild('product') productEl!: ElementRef<HTMLElement>
+
+  constructor(
+    private apiService: ApiService,
+    private actR: ActivatedRoute,
+    private languageService: LanguageService,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.languageService.currentLanguage$.subscribe((language) => {
+      this.currentLang = language
+      this.getPromotionId()
+    })
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.productEl) {
+      const height = this.productEl.nativeElement.offsetHeight
+      const newJustify = height > 30 ? 'center' : 'flex-start'
+      if (newJustify !== this.justifyContent) {
+        this.justifyContent = newJustify
+        this.cd.detectChanges()
+      }
+    }
+  }
+
+  private getPromotionId(): void {
+    this.actR.paramMap.subscribe((parameterMap) => {
+      this.promotionId = Number(parameterMap.get('detail')?.split('-').at(-1))
+      this.loadPromotionDetail(this.currentLang, this.page, 28, this.promotionId)
+    })
+  }
+
+  private loadPromotionDetail(
+    lang: string,
+    page: number,
+    limit: number,
+    promotionId: number,
+    categoryIds?: number[]
+  ): void {
+    this.showLoader = true
+    this.apiService.promotionDetail(lang, page, limit, promotionId, categoryIds).subscribe({
+      next: (data) => {
+        if (this.promotionDetail && page > 1) {
+          this.promotionDetail.items = [...this.promotionDetail.items, ...data.items]
+
+          this.promotionDetail.categories = data.categories
+          this.promotionDetail.item = data.item
+        } else {
+          this.promotionDetail = data
+        }
+        this.showLoader = false
+      },
+      error: (error: unknown) => {
+        console.error(error)
+        this.showLoader = false
+      },
+    })
+  }
+
+  public toggleCategoryId(id: number): void {
+    if (this.promotionId) {
+      this.page = 1
+      this.loadPromotionDetail(this.currentLang, this.page, 28, this.promotionId)
+      if (this.categoryIds.includes(id)) {
+        this.categoryIds.splice(this.categoryIds.indexOf(id), 1)
+      } else {
+        this.categoryIds.push(id)
+      }
+    }
+  }
+
+  public nextPage(): void {
+    if (this.promotionId) {
+      this.page++
+      this.loadPromotionDetail(this.currentLang, this.page, 28, this.promotionId)
+    }
+  }
+}
---
+++ webapp/src/app/promotion/promotion.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { ApiService } from '../services/api.service'
+import { LanguageService } from '../services/language.service'
+import { IPromotion } from '../interfaces/promotion.interface'
+import { ViewportService } from '../services/viewport.service'
+
+@Component({
+  selector: 'app-promotion',
+  imports: [],
+  templateUrl: './promotion.component.html',
+  styleUrl: './promotion.component.scss',
+})
+export class PromotionComponent implements OnInit {
+  public currentLang: 'ka' | 'en' = 'en'
+  public promotion?: IPromotion
+  public viewportWidth
+
+  constructor(
+    private apiService: ApiService,
+    private languageService: LanguageService,
+    private router: Router,
+    private actR: ActivatedRoute,
+    private viewport: ViewportService
+  ) {}
+
+  ngOnInit(): void {
+    this.languageService.currentLanguage$.subscribe((language) => {
+      this.currentLang = language
+      this.loadPromotion(this.currentLang)
+    })
+
+    this.viewport.Viewport$.subscribe((values) => {
+      this.viewportWidth = values.width
+    })
+  }
+
+  private loadPromotion(lang: string): void {
+    this.apiService.promotion(lang).subscribe({
+      next: (data: IPromotion) => {
+        this.promotion = data
+      },
+      error: (error: unknown) => {
+        console.error(error)
+      },
+    })
+  }
+
+  public navigateTo(route: string[]): void {
+    this.router
+      .navigate(route, {
+        relativeTo: this.actR,
+        replaceUrl: false,
+        queryParamsHandling: 'merge',
+      })
+      .catch((error: unknown) => {
+        console.error(error)
+      })
+  }
+}
