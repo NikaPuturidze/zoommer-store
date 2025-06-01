7d565f03d4dc6d4fbc6f5da2716062d6439572bd
NikaPuturidze <nikushaputuridze@gmail.com>
2025-06-01 10:50:27 +0400

    feat: add vertical section to represent current displayed section

--- webapp/src/app/details/additional-info/additional-info.component.ts
+++ webapp/src/app/details/additional-info/additional-info.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core'
 import { AccesoriesComponent } from './accesories/accesories.component'
 import { SimmilarComponent } from './simmilar/similar.component'
 import { FeaturesComponent } from './features/features.component'
@@ -7,7 +16,10 @@ import { BranchesComponent } from './branches/branches.component'
 import { IProduct, IProductResponse } from '../../../interfaces/product.interface'
 import { ContentLoaderModule } from '@ngneat/content-loader'
 import { TranslateModule } from '@ngx-translate/core'
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
+import { fromEvent, throttleTime, map } from 'rxjs'
 
+@UntilDestroy()
 @Component({
   selector: 'app-additional-info',
   imports: [
@@ -21,8 +33,48 @@ import { TranslateModule } from '@ngx-translate/core'
   ],
   templateUrl: './additional-info.component.html',
   styleUrl: './additional-info.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdditionalInfoComponent {
+export class AdditionalInfoComponent implements OnInit {
   @Input() product?: IProduct
   @Input() productResponse?: IProductResponse
+  public isSticky = false
+  public scroll = 0
+  public activeSection = 0
+  sectionsMap: { key: string | null; element: ElementRef<HTMLDivElement> }[] = []
+
+  @ViewChildren('observeSection', { read: ElementRef }) sections?: QueryList<ElementRef<HTMLDivElement>>
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.listenToScroll()
+  }
+
+  public listenToScroll(): void {
+    fromEvent(window, 'scroll')
+      .pipe(
+        throttleTime(10),
+        map(() => window.scrollY),
+        untilDestroyed(this)
+      )
+      .subscribe((scroll) => {
+        this.isSticky = scroll >= 1075
+        this.scroll = scroll
+
+        this.sections?.forEach((section) => {
+          const rect = section.nativeElement.getBoundingClientRect()
+          const offset = 160
+
+          if (rect.top <= offset && rect.bottom > offset) {
+            this.activeSection = Number(section.nativeElement.getAttribute('data-section'))
+          }
+        })
+        const nearBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 10
+        if (nearBottom && this.sections) {
+          this.activeSection = this.sections.length - 1
+        }
+        this.cdr.markForCheck()
+      })
+  }
 }
